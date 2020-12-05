07b76b902b9059581d32b1438b542b98ed369b8f
Rajesh Bhartia <rbhartiamuk@gmail.com>
2020-12-05 11:35:38 +0600

    small modification

--- app.js
+++ app.js
@@ -84,16 +84,15 @@ function createSubMenu(ele, array) {
 			let resp = await onAppQuery('contents', 'content,meta,layout', `WHERE path = '${req.path}'`);
 			// console.log(resp[0]);
 			if (resp.length) {
-				if (resp[0].layout === 'primary')
-					res.render('index', { bodyData: resp[0], response: menuTree });
-				else if (resp[0].layout === 'documentation') {
+				if (resp[0].layout === 'documentation') {
 					let docsResp = await onAppQuery('contents', '*', `WHERE post_type = '${req.path.substring(1)}'`);
 					let catagories = new Set();
 					docsResp.forEach((item, i) => {
 						catagories.add(item.category);
 					})
 					res.render('index', { bodyData: resp[0], response: menuTree, catagories, docsResp });
-				}
+				} else
+					res.render('index', { bodyData: resp[0], response: menuTree });
 			}
 			else next(createError(404, 'This Content does not exist!', { extraProp: "Error Layout Data " }));
 		} catch (error) {
